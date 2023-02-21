fb713c160941d4f7fad49062342efa3fe9b9a4b5
Enrique Fernandez <enriquefernandezchacon@gmail.com>
2023-02-21 20:43:13 +0100

    Prueba 2

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,45 @@
-
-
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-
 import java.io.IOException;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 public class Main {
+
     public static void main(String[] args) {
-        ftp();
+        String server = "ftp.uv.es";
+        int port = 21;
+        String username = "anonymous";
+        String password = "";
+
+        FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.connect(server, port);
+            ftpClient.login(username, password);
+            FTPFile[] files = ftpClient.listFiles("/");
+            for (FTPFile file : files) {
+                System.out.println(file.getName());
+                if (file.isDirectory()) {
+                    listFiles(ftpClient, file.getName());
+                }
+            }
+            ftpClient.logout();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                ftpClient.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    //Conéctate a ftp.uv.es y visualiza los directorios del directorio raíz, entra después en cada directorio del directorio raíz mostrando la lista de ficheros y directorios que hay. Prueba en otros servidores FTP que admiten usuarios anónimos como, ftp.freebsd.org, ftp.unavarra.es, y pon otros ejemplos que encuentres por internet.
-
-    private static void ftp() {
-        FTPClient ftp = new FTPClient();
-        try {
-            ftp.connect("ftp.uv.es");
-            ftp.login("anonymous", "anonymous");
-            ftp.enterLocalPassiveMode();
-            ftp.setFileType(FTP.BINARY_FILE_TYPE);
-            String[] names = ftp.listNames();
-            for (String name : names) {
-                System.out.println(name);
+    private static void listFiles(FTPClient ftpClient, String path) throws IOException {
+        FTPFile[] files = ftpClient.listFiles(path);
+        for (FTPFile file : files) {
+            System.out.println(path + "/" + file.getName());
+            if (file.isDirectory()) {
+                listFiles(ftpClient, path + "/" + file.getName());
             }
-            ftp.logout();
-            ftp.disconnect();
-        } catch (IOException ex) {
-            System.out.println("Error: " + ex.getMessage());
-            ex.printStackTrace();
         }
     }
 }
